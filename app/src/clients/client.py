5fae8e82f69ecf810321ffc64934a51ce2345a9a
Maycol <maycolteles@hotmail.com>
2022-12-22 22:24:41 -0300

    feat: Adding the client class.

---
+++ app/src/clients/client.py
@@ -0,0 +1,13 @@
+"""
+Module containing the "Client" Class.
+"""
+
+from src.addresses import Address
+
+
+class Client:
+    """
+    Class to represent the Client.
+    """
+    _name: str
+    _address: Address
